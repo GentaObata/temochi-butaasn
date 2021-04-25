9b849b9b0a617f4ceb4c25656a9b8641b9d66584
genta obata <g.obata.biz@gmail.com>
2021-04-25 23:11:23 +0900

    豚がスワイプに追従するように

--- temochi-butasan/GameScene.swift
+++ temochi-butasan/GameScene.swift
@@ -13,30 +13,23 @@ class GameScene: SKScene {
     private var label: SKLabelNode?
     private var spinnyNode: SKShapeNode?
     private var pigNode: SKSpriteNode!
+    
+    private var isPigTouching = false
    
     override func didMove(to view: SKView) {
-
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+//        // 青い四角形を作る.
+//        let rect = SKShapeNode(rectOf: CGSize(width: 200.0, height: 300.0))
+//        // 線の色を青色に指定.
+//        rect.strokeColor = UIColor.blue
+//        // 線の太さを2.0に指定.
+//        rect.lineWidth = 2.0
+//        // 四角形の枠組みの剛体を作る.
+//        rect.physicsBody = SKPhysicsBody(edgeLoopFrom: rect.frame)
+//        rect.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+//        self.addChild(rect)
         
         self.pigNode = SKSpriteNode(imageNamed: "pig")
+        self.pigNode.name = "pig"
         let resizeWidth = self.size.width / 3
         let resizeHeight = self.pigNode.size.height * resizeWidth / self.pigNode.size.width
         self.pigNode.size = CGSize(width: resizeWidth, height: resizeHeight)
@@ -46,47 +39,42 @@ class GameScene: SKScene {
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
     }
-    
+        
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        //TODO: タッチした位置と豚の位置の位置関係を保つようにする
+        pigNode.position = pos
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for t in touches {
+            let tochedNode = self.atPoint(t.location(in: self))
+            if tochedNode.name == self.pigNode.name {
+                self.isPigTouching = true
+            }
         }
-
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for t in touches {
+            if isPigTouching {
+                self.touchMoved(toPoint: t.location(in: self))
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        self.isPigTouching = false
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        self.isPigTouching = false
     }
     
     
