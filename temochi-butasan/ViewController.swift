7de569744590be0402081dba38f618c6461c4232
genta obata <g.obata.biz@gmail.com>
2021-04-25 18:08:29 +0900

    Initial Commit

---
+++ temochi-butasan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  temochi-butasan
+//
+//  Created by tilda on 2021/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ temochi-butasanTests/temochi_butasanTests.swift
@@ -0,0 +1,33 @@
+//
+//  temochi_butasanTests.swift
+//  temochi-butasanTests
+//
+//  Created by tilda on 2021/04/25.
+//
+
+import XCTest
+@testable import temochi_butasan
+
+class temochi_butasanTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
