25c65c7eceac52b15a4d824d0b3de26ca7260b5c
genta obata <g.obata.biz@gmail.com>
2021-04-30 00:17:47 +0900

    Noiで豚の形に切り取って生成する

--- temochi-butasan/Node/Butasan.swift
+++ temochi-butasan/Node/Butasan.swift
@@ -11,20 +11,22 @@ class Butasan: SKSpriteNode {
 
     init(categoryBitMask: UInt32, contactTestBitMask: UInt32) {
         let texture = SKTexture(imageNamed: "pig")
-        let resizeWidth = UIScreen.main.bounds.size.width / 3
+        let resizeWidth = UIScreen.main.bounds.size.width / Settings.butaResizeRatio
         let resizeHeight = texture.size().height
             * resizeWidth / texture.size().width
         let size = CGSize(width: resizeWidth, height: resizeHeight)
         super.init(texture: texture, color: .clear, size: size)
+        self.name = "buta"
         
         // テストのための設定
         self.isAccessibilityElement = true
         self.accessibilityIdentifier = "buta"
         
-        self.physicsBody = SKPhysicsBody(rectangleOf: frame.size)
+        self.physicsBody = SKPhysicsBody(texture: texture, size: size)
         self.physicsBody!.velocity = CGVector(dx: 0, dy: 0)
         self.physicsBody!.categoryBitMask = categoryBitMask
         self.physicsBody!.contactTestBitMask = contactTestBitMask
+        self.physicsBody!.friction = Settings.butaFriction
     }
     
     required init?(coder aDecoder: NSCoder) {
