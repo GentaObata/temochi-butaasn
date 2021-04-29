91b97d486f39f162c1d5b9e16a63ce4031f43abf
genta obata <g.obata.biz@gmail.com>
2021-04-30 00:17:08 +0900

    枠の位置の修正

--- temochi-butasan/Node/Area.swift
+++ temochi-butasan/Node/Area.swift
@@ -10,10 +10,13 @@ import SpriteKit
 class Area: SKShapeNode {
     init(categoryBitMask: UInt32) {
         super.init()
-        self.path = CGPath(rect: CGRect(origin: CGPoint(), size: UIScreen.main.bounds.size), transform: nil)
+        self.path = CGPath(rect: CGRect(origin: CGPoint(x: self.frame.minX, y: self.frame.minY), size: UIScreen.main.bounds.size), transform: nil)
+//        self.strokeColor = .gray
+//        self.lineWidth = 16
         self.lineWidth = 0
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
-        self.physicsBody?.categoryBitMask = categoryBitMask
+        self.physicsBody!.categoryBitMask = categoryBitMask
+        self.physicsBody!.restitution = Settings.restitution
     }
     
     required init?(coder aDecoder: NSCoder) {
